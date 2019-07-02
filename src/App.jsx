d8e962024fc7b1df420b95aea3c1b2dffa6d0419
Justin2719 <764836661@qq.com>
2019-07-02 18:43:37 +0800

    登录功能完成

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 //导入路由
-import { HashRouter as Router, Route, Link, Switch, Redirect } from "react-router-dom";
+import { HashRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import Home from './components/Home'
 import Login from './components/Login'
 
@@ -8,14 +8,6 @@ class App extends React.Component{
   render(){
     return (
       <Router>
-        {/* <ul>
-          <li>
-            <Link to="/login">登录</Link>
-          </li>
-          <li>
-            <Link to="/home">首页</Link>
-          </li>
-        </ul> */}
         <Switch>
           <Redirect exact path="/" to="/home" />
           <Route path="/login" component={Login} />
--- src/index.js
+++ src/index.js
@@ -2,10 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.jsx';
+import 'semantic-ui-css/semantic.min.css'
+// 导入axios对象
+import axios from 'axios'
+// 把axios对象绑定到了React组件的原型上，将来所有的react组件都能访问到axios对象
+React.Component.prototype.axios = axios
+axios.defaults.baseURL = 'http://47.96.21.88:8086/'
+
+// 给axios配置一个响应拦截器 直接把data中的数据返回
+axios.interceptors.response.use(
+  function (response) {
+    // 拦截到axios所有的请求，直接返回了响应结果中的data数据
+    return response.data
+  },
+  function (error) {
+    return error
+  }
+)
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 
