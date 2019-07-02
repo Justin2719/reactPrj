a6feea01aef31f33033d39149408d8bf6a5e65e3
Justin2719 <764836661@qq.com>
2019-07-02 16:34:44 +0800

    路由配置

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+//导入路由
+import { HashRouter as Router, Route, Link, Switch, Redirect } from "react-router-dom";
+import Home from './components/Home'
+import Login from './components/Login'
 
 class App extends React.Component{
   render(){
     return (
-      <div>这是根组件</div>
+      <Router>
+        {/* <ul>
+          <li>
+            <Link to="/login">登录</Link>
+          </li>
+          <li>
+            <Link to="/home">首页</Link>
+          </li>
+        </ul> */}
+        <Switch>
+          <Redirect exact path="/" to="/home" />
+          <Route path="/login" component={Login} />
+          <Route path="/home" component={Home} />
+        </Switch>
+      </Router>
     )
   }
 }
---
+++ src/components/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div>这是login组件</div>
+    )
+  }
+}
+
+export default Login
